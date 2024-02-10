78cc5d2e7ba57d4edcbd61388c85af29ccd74b73
IamNormi <automatecode@gmail.com>
2024-02-10 09:15:41 +0530

    cursor

--- config.h
+++ config.h
@@ -151,7 +151,7 @@ unsigned int defaultunderline = 7;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 4;
+static unsigned int cursorshape = 2;
 
 /*
  * Default columns and rows numbers
